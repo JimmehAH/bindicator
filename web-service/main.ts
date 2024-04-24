17e389887971174ca49e297864b29903dfbe19f4
James Hannett <jameshannett@gmail.com>
2024-04-24 14:53:08 +0100

    Protect incoming webhook with basic authentication

--- web-service/main.ts
+++ web-service/main.ts
@@ -1,6 +1,18 @@
 import { Hono } from "https://deno.land/x/hono@v4.2.7/mod.ts";
+import { basicAuth } from "https://deno.land/x/hono@v4.2.7/middleware.ts";
+import { load } from "https://deno.land/std@0.223.0/dotenv/mod.ts";
 
+const env = await load();
 const app = new Hono();
+
+// Protect the incoming webhook
+app.use(
+  "/incoming",
+  basicAuth({
+    username: env["BASIC_USER"],
+    password: env["BASIC_PASSWORD"],
+  }),
+);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
