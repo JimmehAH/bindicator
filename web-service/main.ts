b6f621653018ba2e0e6bcd55aaf01c5854769631
James Hannett <jameshannett@gmail.com>
2024-05-08 15:06:09 +0100

    Add a MicroPython timestamp type and use it when necessary

--- web-service/main.ts
+++ web-service/main.ts
@@ -11,6 +11,18 @@ const kv = await Deno.openKv();
 
 type CollectionType = { collection: string; colour: Color };
 
+// see https://docs.micropython.org/en/latest/library/time.html#time.mktime for details of this 8-tuple
+type MicropythonTimestamp = {
+  year: number; // year includes the century (for example 2014).
+  month: number; // month is 1-12
+  mday: number; // mday is 1-31
+  hour: number; // hour is 0-23
+  minute: number; // minute is 0-59
+  second: number; // second is 0-59
+  weekday: number; // weekday is 0-6 for Mon-Sun
+  yearday: number; // yearday is 1-366
+};
+
 // see https://dsteinman.github.io/ts-color-class/colors.html for colour reference
 const COLLECTION_TYPES: CollectionType[] = [
   { collection: "RECYCLING", colour: new Color("blue") },
@@ -19,7 +31,11 @@ const COLLECTION_TYPES: CollectionType[] = [
   { collection: "FOOD WASTE", colour: new Color("green 3") },
 ];
 
-type WasteCollection = { date: Date; collections: CollectionType[] };
+type WasteCollection = {
+  date: Date;
+  mp_date?: MicropythonTimestamp;
+  collections: CollectionType[];
+};
 
 /**
  * TODO: actually parse the email and return a sensible value based on that
@@ -47,6 +63,22 @@ function find_collections_in_email(email_body: string): CollectionType[] {
   );
 
   return collections;
+}
+
+function convert_to_micropython_timestamp(
+  timestamp: Date,
+): MicropythonTimestamp {
+  const mp_timestamp: MicropythonTimestamp = {
+    year: timestamp.getFullYear(),
+    month: timestamp.getMonth(),
+    mday: timestamp.getDate(),
+    hour: timestamp.getHours(),
+    minute: timestamp.getMinutes(),
+    second: timestamp.getSeconds(),
+    weekday: timestamp.getDay(),
+    yearday: 0, // I don't care about yearday, or weekday tbh
+  };
+  return mp_timestamp;
 }
 
 // Protect the routes
@@ -80,6 +112,14 @@ app.post("/incoming", async (c) => {
 app.get("/next-collection", async (c) => {
   const next_collecton = (await kv.get(["next_collection", env["BASIC_USER"]]))
     .value as WasteCollection;
+
+  // check to see if it's our dedicated device that is making the request
+  // if so then we must transform the timestamps to a form that is easier to use in micropython
+  if (c.req.header("User-Agent")?.search(/Bindicator/)) {
+    next_collecton.mp_date = convert_to_micropython_timestamp(
+      next_collecton.date,
+    );
+  }
 
   return c.json(next_collecton);
 });
